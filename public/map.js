4afe2c854eb87c2696eb6bb55daed82ecd3d26ff
Alan Verga <alan.verga@gmail.com>
2013-04-29 22:46:24 -0400

    updated map w/ interaction & basic styling

--- public/map.js
+++ public/map.js
@@ -1,18 +1,137 @@
 window.onload = function() {
-    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    function slugify(text) {
+	text = text.replace(/[^-a-zA-Z0-9,&\s]+/ig, '');
+	text = text.replace(/-/gi, "_");
+	text = text.replace(/\s/gi, "-");
+	return text;
+    }
+
+    //populate data
+    $.each(ourcityFeatures.features, function(name, value) {
+	$('#neighborhoods').append(
+	    '<li id="' + slugify(value.properties.name) + '">' + 
+		'<a href=#>' + value.properties.name + '</a></li>');
+
+    });
 
 
-    // add an OpenStreetMap tile layer
+    //initalize map
+    var map = L.map('map',  {
 
+	/*options to dicuss
+	scrollWheelZoom: false,
+	touchZoom: false,
+        doubleClickZoom: false,
+        zoomControl: false,
+        dragging: false,
+*/
+        maxZoom:16,
+        minZoom:8
+    });
+    
+
+    //center
+    map.setView([32.7153292, -117.1572551], 11);
+
+    /*tiles*/
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // add a marker in the given location, attach some popup content to it and open the popup
 
-    L.marker([51.5, -0.09]).addTo(map)
-	.bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
-	.openPopup();
-}
+    /*geojson layer - declared now for styling access*/
+    var geojson;
 
 
+    /* Control */
+    var info = L.control({position: 'bottomleft'});
+
+    info.onAdd = function (map) {
+	this._div = L.DomUtil.create('div', 'info');
+	this.update();
+	return this._div;
+    };
+
+    // method that we will use to update the control based on feature properties passed
+
+    info.update = function (props) {
+	this._div.innerHTML = (props ?
+			       '<b>' + props.name + '</b><br />' :'' );
+    };
+
+    //info.addTo(map);  //moved reset/highlight
+    
+    /* Styles: Neighborhood */
+    var defaultStyle = {
+        color: "#2262CC",
+        weight: 2,
+        opacity: 0.6,
+        fillOpacity: 0.1,
+        fillColor: "#2262CC"
+    };
+    
+    var highlightStyle = {
+        color: '#2262CC', 
+        weight: 3,
+        opacity: 0.6,
+        fillOpacity: 0.65,
+        fillColor: '#2262CC'
+    };
+
+    function highlightFeature(e) {
+	var layer = e.target;
+
+	layer.setStyle(highlightStyle);
+
+	if (!L.Browser.ie && !L.Browser.opera) {
+            layer.bringToFront();
+	}
+	info.addTo(map);
+	info.update(layer.feature.properties);
+    }
+
+
+    function resetHighlight(e) {
+	geojson.resetStyle(e.target);
+	//info.update();
+	info.removeFrom(map);
+    }
+
+    function zoomToFeature(e) {
+	map.fitBounds(e.target.getBounds());
+    }
+
+    function eachFeature(feature, layer) {
+
+	layer.on({
+	    mouseover: highlightFeature,
+	    mouseout: resetHighlight,
+	    click: zoomToFeature
+	});
+
+
+	$('#' + slugify(feature.properties.name)).hover(
+	    function() {
+		layer.fire("mouseover"); }, 
+	    function() {
+		layer.fire("mouseout"); }
+	);
+
+	$('#' + slugify(feature.properties.name)).click( 
+	    function() {
+		layer.fire("click");
+	    }
+	);
+
+    }
+
+    geojson = L.geoJson(ourcityFeatures, {
+	style: defaultStyle,
+	onEachFeature: eachFeature
+    }).addTo(map);
+
+	
+
+
+} //end window onload
