d44a0e6eeb7f9120f6bf0bc65b12ce34b6d3bee6
Alan Verga <alan.verga@gmail.com>
2013-04-30 00:32:49 -0400

    trying out assetpack

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
 gem 'thin'
--- app.rb
+++ app.rb
@@ -1,6 +1,36 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/assetpack'
 
-get '/' do
-  send_file File.join(settings.public_folder, 'index.html')
+class App < Sinatra::Base
+  set :root, File.dirname(__FILE__) # You must set app root
+
+
+  register Sinatra::AssetPack
+
+  assets do
+
+    js :app, '/js/app.js', [
+      '/js/*.js'                     
+    ]
+
+    css :application, '/css/application.css', [
+      '/css/app.css'
+    ]
+
+    js_compression  :jsmin    # :jsmin | :yui | :closure | :uglify
+    css_compression :simple   # :simple | :sass | :yui | :sqwish
+    
+  end
+
+  
+  get '/' do
+    erb :index
+  end
+
 end
+
+
+
+
+
 
